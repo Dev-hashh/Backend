b7efe9d78fb0b15d76d3450e07ceb4369e445874
Dev-hashh <devk07322@gmail.com>
2025-09-01 00:00:01 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
